a759d8d4ed9012a10fdd4198fd347b6b7972f792
flywind <xzsflywind@gmail.com>
2020-03-30 22:52:42 +0800

    queue init

---
+++ asyncio.nimble
@@ -0,0 +1,13 @@
+# Package
+
+version       = "0.1.0"
+author        = "flywind"
+description   = "Basic data structures for async Nim."
+license       = "Apache License"
+srcDir        = "src"
+
+
+
+# Dependencies
+
+requires "nim >= 1.0.0"
---
+++ src/asyncio/queue.nim
@@ -0,0 +1,65 @@
+type
+  CircularQueue*[N: static[int], T] = object
+    data: array[N, T]
+    head, tail: int
+
+proc newCircularQueue*[N: static[int], T](): CircularQueue[N, T] =
+  discard
+
+func isFull*(q: CircularQueue): bool {.inline.} =
+  abs(q.tail-q.head) == q.N
+
+func isEmpty*(q: CircularQueue): bool {.inline.} =
+  q.tail == q.head
+
+proc deQueue*[N: static[int], T](q: var CircularQueue[N, T]): owned T =
+  if q.isEmpty:
+    return
+  if q.head < q.N:
+    result = move q.data[q.head]
+  else:
+    result = move q.data[q.head-q.N]
+  inc(q.head)
+  if q.head == 2 * q.N:
+    q.head = 0
+
+proc peekQueue*[N: static[int], T](q: CircularQueue[N, T]): lent T =
+  if q.isEmpty:
+    return
+  if q.head < q.N:
+    result = q.data[q.head]
+  else:
+    result = q.data[q.head-q.N]
+
+proc enQueue*[N: static[int], T](q: var CircularQueue[N, T], v: sink T) =
+  if q.isFull:
+    return
+  if q.tail < q.N:
+    q.data[q.tail] = v
+  else:
+    q.data[q.tail-q.N] = v
+  inc(q.tail)
+  if q.tail == 2 * q.N:
+    q.tail = 0
+
+func len*(q: CircularQueue): int {.inline.} =
+  result = q.tail - q.head
+  while result < 0:
+    result.inc(q.N)
+
+
+when isMainModule:
+  var c = newCircularQueue[10, int]()
+  for i in 1 .. 10:
+    c.enQueue(i)
+  for i in 1 .. 10:
+    discard c.deQueue
+  for i in 1 .. 10:
+    c.enQueue(i)
+  for i in 1 .. 8:
+    discard c.deQueue
+
+  echo c.peekQueue
+  echo c.deQueue
+  echo c.data
+  echo c.len
